996836a7f3dce383b3cb215955bd58ea1951fe9b
Becca Barton <becca.barton@voxmedia.com>
2018-05-02 21:19:42 -0400

    add auth even tho i really didnt want to

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+  include TokenAuthenticatable
+  include AdminAuthorizable
+
+  rescue_from ActiveRecord::RecordNotFound, with: -> { render json: { error: 'Sry Ed Sheeran fan, that record wasn\'t found }, status: :not_found }
 end
