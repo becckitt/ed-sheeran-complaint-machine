76282faebc1ef22f15088abfa569b2bd720d936e
Becca Barton <becca.barton@voxmedia.com>
2018-05-05 23:31:19 -0400

    some cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
-  include TokenAuthenticatable
-  include AdminAuthorizable
+  # include TokenAuthenticatable
+  # include AdminAuthorizable
 
-  rescue_from ActiveRecord::RecordNotFound, with: -> { render json: { error: 'Sry Ed Sheeran fan, that record wasn\'t found }, status: :not_found }
+  rescue_from ActiveRecord::RecordNotFound, with: -> { render json: { error: 'Sry Ed Sheeran fan, that record wasn\'t found' }, status: :not_found }
 end
