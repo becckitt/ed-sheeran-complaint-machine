29b1a39c3c67b122f0a7a8ddd577f1f68200fdae
Becca Barton <becca.barton@voxmedia.com>
2018-05-05 23:39:34 -0400

    first structure of frontend

---
+++ ed-sheeran-complaint-machine-frontend/src/components/ComplaintForm.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'ComplaintForm',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- ed-sheeran-complaint-machine-frontend/src/router/index.js
+++ ed-sheeran-complaint-machine-frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import ComplaintMachine from '@/components/views/ComplaintMachine'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Ed Sheeran Complaint Machine',
+      component: ComplaintMachine
     }
   ]
 })
